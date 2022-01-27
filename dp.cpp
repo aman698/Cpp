2b208a346355d36e7a53dcc6e6170a4e04262083
aman698 <78063998+aman698@users.noreply.github.com>
2022-01-27 21:38:26 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -80,6 +80,41 @@ int minSteps(int n){
     int ans = min(x, min(y,z)) + 1;
 }
 
+// MINSTEP TO 1 OPTIMAIZATION   T.C => O(n)
+
+int minStepsHelper(int n, int *ans){
+    // Base Case
+    if(n<=1){
+        return 0;
+    }
+
+    // Check if output already exists
+    if(ans[n] != -1){
+        return ans[n];
+    }
+
+    // Calculate output
+    int x = minStepsHelper(n-1);
+
+    int y = INT_MAX, z = INT_MAX;
+    if(n%2 == 0){
+        y = minStepsHelper(n/2, ans);
+    }
+    if(n%3 == 0){
+        z = minStepsHelper(n/3, ans);
+    }
+    int output = min(x,min(y,z)) + 1;
+    return output;
+}
+
+int minSteps_2(int n){
+    int *ans = new int[n+1];
+    for(int i=0;i<=n;i++){
+        ans[i] = -1;
+    }
+    return minStepsHelper(n,ans);
+}
+    
 int main(){
     int n;
     cin>>n;
