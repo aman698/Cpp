67d617cf372e17828bc973862ad82249ac877b42
aman698 <78063998+aman698@users.noreply.github.com>
2022-01-28 13:49:56 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -197,6 +197,91 @@ int main(){
     }
     cout<<minCostPath(input,m,n)<<endl;
 }
+// ishi ka ha
+// MINCLOSE PATH
+#include<bits/stdc++.h>
+using namespace std;
+
+// Dhynamic programmming
+int minCostPath_DP(int **input, int m, int n){
+    int **output = new int*[m];
+    for(int i=0;i<m;i++){
+        output[i] = new int[n];
+    }
+
+    // Fill the last cell i.e. destination
+    output[m-1][n-1] = input[m-1][n-1];
+
+    // Fill the last row (right to left)
+    for(int j = n-2; j >= 0; j--){
+        output[m-1][j] = output[m-1][j+1] + input[m-1][j];
+    }
+    // Fill the last column (bottom to top)
+    for(int i = m-2; i >= 0; i--){
+        output[i][n-1] = output[i+1][n-1] + input[i][n-1];
+    }
+    // Fill remaining cells
+    for(int i = m-2; i >= 0; i--){
+        for(int j = n-2;j >= 0; j--){
+            output[i][j] = min(output[i][j+1],min(output[i+1][j+1], output[i+1][j])) input[i][j];
+        }
+    }
+    return output[0][0];
+}
+// MEMOIZATION
+int minCostPath_Mem(int **input, int m, int n, int i, int j, int **output){
+    if(i == m-1 && j == n-1){
+    return input[i][j];
+    }
+    if(i >= m || j >= n){
+        return INT_MAX;
+    }
+
+    // Check if ans already exists
+    if(output[i][j] != -1){
+        return output[i][j];
+    }
+
+    // Recursive calls
+    int x = minCostPath_Mem(input,m,n,i,j+1,output);
+    int y = minCostPath_Mem(input,m,n,i+1,j+1,output);
+    int z = minCostPath_Mem(input,m,n,i+1,j,output);
+
+    // Small Calculation
+    int a = min(min, min(y,z)) + input[i][j];
+
+    //save ker leta h
+    output[i][j] = a;
+    return a;
+}
+
+int minCostPath(int **input, int m, int n){
+    int **output = new int*[m];
+    for(int i=0;i<m;i++){
+        output[i] = new int[n];
+        for(int j=0;j<n;j++){
+            output[i][j] = -1;
+        }
+    }
+
+    return minCostPath(input,m,n,i,j,output);
+}
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int **input = new int*[m];
+    for(int i=0;i<m;i++){
+        input[i] = new int[n];
+    
+    for(int j=0;j<n;j++){
+        cin>>input[i][j];
+    }
+    }
+    cout<<minCostPath_Mem(input,m,n,0,0)<<endl;
+    cout<<minCostPath_Mem(input,m,n)<<endl;
+    cout<<minCostPath(input,m,n);
+}
 
 
 
