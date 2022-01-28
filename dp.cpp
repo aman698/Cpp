95e2b5404d04409f43b6eb4d69a9e808a69e6112
aman698 <78063998+aman698@users.noreply.github.com>
2022-01-28 18:39:30 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -283,6 +283,173 @@ int main(){
     cout<<minCostPath(input,m,n);
 }
 
+// knapsack
+//brute force   T.C => O(2^k)
+#include<bits/stdc++.h>
+using namespace std;
+
+// A utility function that returns
+//maximum of two numbers
+int max(int a, int b){
+    return (a>b)?a:b;
+}
+int knapSack(int W, int wt[], int val[], int n){
+    // Base case
+    if( n == 0 || W == 0)
+    return 0;
+    
+    // If weight of the nth item is more
+    // than knapsack capacity W, then,
+    //this item cannot be included
+    // in the optimal solution
+    if(wt[n-1]>W)
+    return knapSack(W,wt,val,n-1);
+    /* Return the maximum of two cases:
+    1. nth item included
+    2. not included
+    */
+    else
+        return max(
+            val[n-1]
+                + knapSack(W - wt[n-1],wt,val,n-1), knapSack(W,wt,val,n-1));
+}
+
+int main(){
+    int val[] = {60,100,120};
+    int wt[] = {10,20,30};
+    int W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    cout<<knapSack(W,wt,val,n);
+    return 0;
+}
+
+// Memomiazation
+#include<bits/stdc++.h>
+using namespace std;
+
+// Returns the value of maximum profit
+int knapSackRec(int W, int wt[], int val[], int i, int** dp){
+    // base condition
+    if(i<0)
+    return 0;
+    if(dp[i][W] != -1)
+    return dp[i][W];
+
+    if(wt[i] > W){
+        // Store the value of function call
+        //stack in table before return
+        dp[i][W] = knapSackRec(W,wt,val,i-1,dp);
+        return dp[i][W];
+    }
+    else{
+        // Store value in a table brfore return
+        dp[i][W] = max(val[i] + knapSackRec(W - wt[i], wt, val, i-1,dp), knapScakRec(W,wt,val,i-1,dp));
+        
+        // Return value of table after storing
+        return dp[i][W];
+    }
+}
+
+int knapSack(int W, int wt[], int val[], int n){
+    int** dp;
+    dp = new int*[n];
+
+    for(int i=0;i<n;i++)
+    dp[i] = new int[W+1];
+
+    for(int i=0;i<n;i++)
+    dp[i] = new int[W+1];
+
+    for(int i=0;i<n;i++)
+    for(int j=0;j<W+1;j++)
+    dp[i][j] = -1;
+    return knapSackRec(W, wt, val, n-1, dp);
+}
+
+// Driver Code
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    cout << knapSack(W, wt, val, n);
+    return 0;
+}
+
+// USE DP means bottom up
+#include<bits/stdc++.h>
+using namespace std;
+
+// A utility function that returns
+//maximum of two numbers
+//T.C => O(N*W)
+// A.S => O(N*W) 
+int max(int a, int b){
+    return (a>b)?a:b;
+}
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+      vector<vector<int>> K(n + 1, vector<int>(W + 1));
+ 
+    // Build table K[][] in bottom up manner
+    for(i = 0; i <= n; i++)
+    {
+        for(w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] +
+                                K[i - 1][w - wt[i - 1]],
+                                K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+    return K[n][W];
+}
+
+int main(){
+    int val[] = {60,100,120};
+    int wt[] = {10,20,30};
+    int W = 50;
+    int n = sizeof(val)/sizeof(val[0]);
+    cout<<knapSack(W,wt,val,n);
+    return 0;
+}
+
+
+// best dp approach T.C = O(N*W)
+#include<bits/stdc++.h>
+using namespace std;
+
+int knapSack(int W, int wt[], int val[], int n){
+    int dp[W+1];
+    memset(dp,0,sizeof(dp));
+
+    for(int i=1;i<n+1;i++){
+        for(int w = W; w >= 0; w--){
+
+            if(wt[i-1] <= w)
+            // finding the maximum value
+            dp[w] = max(dp[w],dp[w - wt[i-1]] + val[i-1]);
+        }
+    }
+    return dp[W];
+}
+
+int main(){
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    cout << knapSack(W, wt, val, n);
+    return 0;
+}
+
 
 
 
