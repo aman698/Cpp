93d94c71ec1456de61d01dd0ab12de4baeb1cacf
aman698 <78063998+aman698@users.noreply.github.com>
2022-01-28 11:02:00 +0530

    Update dp.cpp

--- dp.cpp
+++ dp.cpp
@@ -159,6 +159,45 @@ int main(){
     cout<<balancedBTs_2(h)<<endl;
 }
 
+// Min Cost Path
+// Brute force
+#include<bits/stdc++.h>
+using namespace std;
+
+int minCostPath(int **input, int m, int n, int i, int j){
+    // Base Case
+    if(i == m-1 && j == n-1){
+        return input[i][j];
+    }
+    if(i >= m || j >= n){
+        return INT_MAX;
+    }
+    // Recursive calls
+    int x = minCostPath(input,m,n,i,j+1);
+    int y = minCostPath(input,m,n,i+1,j+1);
+    int z = minCostPath(input,m,n,i+1,j);
+
+    // Small Calculation
+    int ans = min(x, min(y, z)) + input[i][j];
+}
+
+int minCostPath(int **input, int m, int n){
+    return minCostPath(input,m,n,0,0);
+}
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int **input = new int*[m];
+    for(int i=0;i<m;i++){
+        input[i] = new int[n];
+        for(int j=0;j<n;j++){
+            cin>>input[i][j];
+        }
+    }
+    cout<<minCostPath(input,m,n)<<endl;
+}
+
 
 
 
